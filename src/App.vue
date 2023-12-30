3d22f0a5c1495e15f207433f168985beb8d8a6bc
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-12-30 20:51:40 +0600

    Added popup component, HemodialysisSession.vue component, some styles.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-
+    <HemodialysisSession/>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import HemodialysisSession from "@/components/HemodialysisSession.vue";
 </script>
 
 <style scoped>
---
+++ src/components/popup/v-popup.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="v-popup">
+
+    <div class="v-popup__header">
+      <span>Лекарственные препараты</span>
+      <button class="close_modal" @click="onClosePopup">Закрыть</button>
+    </div>
+
+    <div class="v-popup__content">
+      <slot></slot>
+      <form>
+        <input placeholder="Поиск позиции по первым символам">
+        <button class="material-icons">search</button>
+        <input placeholder="Добавить новую запись">
+        <button class="material-icons">add</button>
+        <input placeholder="Игла размер No1">
+        <input placeholder="Игла размер No2">
+        <input placeholder="Игла размер No3">
+        <input placeholder="Игла размер No4">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "v-popup",
+  props: {
+    onClose: Function,
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    onClosePopup() {
+  this.$emit('onClose')
+    },
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+.v-popup {
+  padding: 16px;
+  position: fixed;
+  top: 50px;
+  width: 400px;
+  border: 1px solid #9e9c9c;
+  background: #ffffff;
+  box-shadow: 0 0 17px 0 #e7e7e7;
+  z-index: 10;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
